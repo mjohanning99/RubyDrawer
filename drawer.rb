eb5370395acc4c37623bee90aabd22fe0c705d2c
Marvin Johanning <manimusmuellus@gmail.com>
2014-01-05 21:07:05 +0100

    The first Version of Drawer
    Drawer! Is a paint program made by Marvin Johanning. This program was
    firtly designed for my 6 year old sister, so that she can learn how to
    use the mouse etc. But I will provide the source code of the program for
    everybody to use. The program was made with the Ruby Gem Gosu and
    TexPlay.

---
+++ drawer.rb
@@ -0,0 +1,187 @@
+require 'gosu'
+require 'texplay'
+
+class Window < Gosu::Window
+	def initialize
+		#super 1022, 600, true, update_interval=2
+		super(1022, 600, false)
+		@background = TexPlay.create_blank_image(self, 1022, 580)
+		$colour = :black
+		$colour1 = :black
+		print "Brush Size: "
+		$size = gets.chomp.to_f
+		@cursor = Gosu::Image.new(self, "lib/brush.png", false)
+		@delete = Gosu::Image.new(self, "lib/delete.png", false)
+		@paint = true
+		@deleting = false
+		@lineing = false
+		@punkt = true
+		@font = Gosu::Font.new(self, Gosu::default_font_name, 20)
+	end
+
+
+	#def needs_cursor?
+	#	true
+	#end
+
+	def update
+		#if button_down? Gosu::KbF1 then $colour1 = :black end
+		#if button_down? Gosu::KbF2 then $colour1 = :red end
+		#if button_down? Gosu::KbF3 then $colour1 = :blue end
+		#if button_down? Gosu::KbF4 then $colour1 = :green end
+		#if button_down? Gosu::KbF5 then $colour1 = :orange end
+		#if button_down? Gosu::KbF6 then $colour1 = :cyan end
+		#if button_down? Gosu::KbF7 then $colour1 = :yellow end
+		#if button_down? Gosu::KbF8 then $colour1 = :turquoise end
+		#if button_down? Gosu::KbF9 then $colour1 = :tyrian end
+		#if button_down? Gosu::KbF10 then $colour1 = :brown end
+	end
+
+	def draw
+		@font.draw("Brush Size: #{$size}", 10, 580, 3, 1.0, 1.0, 0xff00ffff)
+		@font.draw("Colour: #{$colour}", 200, 580, 3, 1.0, 1.0, 0xff00ffff)
+		@font.draw("Colours: black, red, blue, green, orange, cyan, yellow, turquoise, tyrian, brown", 360, 580, 3, 1.0, 1.0, 0xff00ffff)
+		if @paint == true then
+		@cursor.draw(mouse_x, mouse_y - 16, 1)
+	    end
+	    if @deleting == true then
+	    	@delete.draw(mouse_x, mouse_y - 16, 1)
+	    end
+		@background.draw(0, 0, 0)
+		#@background.paint {
+		#	circle 0, 0, 2000, :color => :white, :fill => true
+		#}
+
+		if button_down? Gosu::Kb1 then $colour = :black end
+		if button_down? Gosu::Kb2 then $colour = :red end
+		if button_down? Gosu::Kb3 then $colour = :blue end
+		if button_down? Gosu::Kb4 then $colour = :green end
+		if button_down? Gosu::Kb5 then $colour = :orange end
+		if button_down? Gosu::Kb6 then $colour = :cyan end
+		if button_down? Gosu::Kb7 then $colour = :yellow end
+		if button_down? Gosu::Kb8 then $colour = :turquoise end
+		if button_down? Gosu::Kb9 then $colour = :tyrian end
+		if button_down? Gosu::Kb0 then $colour = :brown end
+
+
+		#if button_down? Gosu::MsWheelUp then $size += 1 end
+		#if button_down? Gosu::MsWheelDown then $size -= 1 end
+
+		$x = mouse_x
+		$y = mouse_y
+		def button_down(id)
+			if id == Gosu::MsWheelUp then $size += 1 end
+			if id == Gosu::MsWheelDown then $size -= 1 end
+		    if id == Gosu::KbUp then $size += 1 end
+		    if id == Gosu::KbDown then $size -= 1 end
+
+			#if id == Gosu::KbF2 then
+			#	Window::screenshot.save("screenshot.png")
+			#end
+			#if id == Gosu::KbR then $colour = :red end
+			#if id == Gosu::KbB then $colour = :black end
+			#if id == Gosu::KbG then $colour = :green end
+			if id == Gosu::KbLeft then
+			   puts "Colours: green, red, blue, black, orange, cyan, purple, yellow, turquoise, tyrian, brown, yellow"
+			   print "What colour: "
+			   colour = gets.chomp!
+			   case colour
+			   when "green"
+			   	$colour = :green
+			   when "red"
+			   	$colour = :red
+			   when "blue"
+			   	$colour = :blue
+			   when "black"
+			   	$colour = :black
+			   when "orange"
+			   	$colour = :orange
+			   when "cyan"
+			   	$colour = :cyan
+			   when "purple"
+			   	$colour = :purple
+			   when "yellow"
+			   	$colour = :yellow
+			   when "turquoise"
+			   	$colour = :turquoise
+			   when "tyrian"
+			   	$colour = :tyrian
+			   when "brown"
+			   	$colour = :brown
+			   end
+			end
+
+			#if id == Gosu::KbUp then $size += 1 end
+			#if id == Gosu::KbDown then $size -= 1 end
+
+			if id == Gosu::KbRight then
+				print "Brush Size: "
+		        $size = gets.chomp.to_f
+		    end
+		    if id == Gosu::KbF then
+		    puts "Colours: green, red, blue, black, orange, cyan, purple, yellow, turquoise, tyrian, brown, yellow"
+			   print "What colour: "
+			   colour = gets.chomp!
+			   case colour
+			   when "green"
+			   	$colour1 = :green
+			   when "red"
+			   	$colour1 = :red
+			   when "blue"
+			   	$colour1 = :blue
+			   when "black"
+			   	$colour1 = :black
+			   when "orange"
+			   	$colour1 = :orange
+			   when "cyan"
+			   	$colour1 = :cyan
+			   when "purple"
+			   	$colour1 = :purple
+			   when "yellow"
+			   	$colour1 = :yellow
+			   when "turquoise"
+			   	$colour1 = :turquoise
+			   when "tyrian"
+			   	$colour1 = :tyrian
+			   when "brown"
+			   	$colour1 = :brown
+			   end
+			   @background.paint {
+			      circle 20, 0, 5000, :color => $colour1, :fill => true
+		       }
+		    end
+		end
+		if button_down? Gosu::MsLeft then
+		@paint = true
+		@deleting = false
+		@background.paint {
+			circle $x, $y, $size, :color => $colour, :fill => true
+			#line $x, $y, $x, $size, :color => $colour, :fill => true
+ 		}
+	    end
+	    if button_down? Gosu::MsRight then
+	    @paint = false
+		@deleting = true
+		@background.paint {
+			circle $x, $y, $size + 2, :color => $colour1, :fill => true
+		}
+	    end
+	    if button_down? Gosu::MsMiddle then
+	    		$colour1 = :white
+	    		@background.paint {
+			      circle 0, 0, 1500, :color => :white, :fill => true
+		}
+	end
+	if @deleting == true then
+		if button_down? Gosu::KbRightShift then
+		@background.paint {
+			circle $x, $y, $size + 2, :color => :black, :fill => false
+			#circle $x, $y, $size + 2, :color => $colour1, :fill => false
+		}
+	end
+	end
+end
+end
+
+window = Window.new
+window.show
