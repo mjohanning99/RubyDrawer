ea500fd3940ef0984334bc06a5af7e4d2e2b5ca3
Jeytas <manimusmuellus@gmail.com>
2014-01-05 21:15:20 +0100

    Update drawer.rb

--- drawer.rb
+++ drawer.rb
@@ -3,19 +3,16 @@ require 'texplay'
 
 class Window < Gosu::Window
 	def initialize
-		#super 1022, 600, true, update_interval=2
 		super(1022, 600, false)
 		@background = TexPlay.create_blank_image(self, 1022, 580)
-		$colour = :black
-		$colour1 = :black
+		$colour = :black #Default brush colour
+		$colour1 = :black #Default background colour
 		print "Brush Size: "
 		$size = gets.chomp.to_f
 		@cursor = Gosu::Image.new(self, "lib/brush.png", false)
 		@delete = Gosu::Image.new(self, "lib/delete.png", false)
 		@paint = true
 		@deleting = false
-		@lineing = false
-		@punkt = true
 		@font = Gosu::Font.new(self, Gosu::default_font_name, 20)
 	end
 
@@ -25,16 +22,6 @@ class Window < Gosu::Window
 	#end
 
 	def update
-		#if button_down? Gosu::KbF1 then $colour1 = :black end
-		#if button_down? Gosu::KbF2 then $colour1 = :red end
-		#if button_down? Gosu::KbF3 then $colour1 = :blue end
-		#if button_down? Gosu::KbF4 then $colour1 = :green end
-		#if button_down? Gosu::KbF5 then $colour1 = :orange end
-		#if button_down? Gosu::KbF6 then $colour1 = :cyan end
-		#if button_down? Gosu::KbF7 then $colour1 = :yellow end
-		#if button_down? Gosu::KbF8 then $colour1 = :turquoise end
-		#if button_down? Gosu::KbF9 then $colour1 = :tyrian end
-		#if button_down? Gosu::KbF10 then $colour1 = :brown end
 	end
 
 	def draw
@@ -48,10 +35,8 @@ class Window < Gosu::Window
 	    	@delete.draw(mouse_x, mouse_y - 16, 1)
 	    end
 		@background.draw(0, 0, 0)
-		#@background.paint {
-		#	circle 0, 0, 2000, :color => :white, :fill => true
-		#}
-
+		
+		#Selecting colour via the Keyboard 
 		if button_down? Gosu::Kb1 then $colour = :black end
 		if button_down? Gosu::Kb2 then $colour = :red end
 		if button_down? Gosu::Kb3 then $colour = :blue end
@@ -64,60 +49,14 @@ class Window < Gosu::Window
 		if button_down? Gosu::Kb0 then $colour = :brown end
 
 
-		#if button_down? Gosu::MsWheelUp then $size += 1 end
-		#if button_down? Gosu::MsWheelDown then $size -= 1 end
-
 		$x = mouse_x
 		$y = mouse_y
 		def button_down(id)
 			if id == Gosu::MsWheelUp then $size += 1 end
 			if id == Gosu::MsWheelDown then $size -= 1 end
-		    if id == Gosu::KbUp then $size += 1 end
-		    if id == Gosu::KbDown then $size -= 1 end
-
-			#if id == Gosu::KbF2 then
-			#	Window::screenshot.save("screenshot.png")
-			#end
-			#if id == Gosu::KbR then $colour = :red end
-			#if id == Gosu::KbB then $colour = :black end
-			#if id == Gosu::KbG then $colour = :green end
-			if id == Gosu::KbLeft then
-			   puts "Colours: green, red, blue, black, orange, cyan, purple, yellow, turquoise, tyrian, brown, yellow"
-			   print "What colour: "
-			   colour = gets.chomp!
-			   case colour
-			   when "green"
-			   	$colour = :green
-			   when "red"
-			   	$colour = :red
-			   when "blue"
-			   	$colour = :blue
-			   when "black"
-			   	$colour = :black
-			   when "orange"
-			   	$colour = :orange
-			   when "cyan"
-			   	$colour = :cyan
-			   when "purple"
-			   	$colour = :purple
-			   when "yellow"
-			   	$colour = :yellow
-			   when "turquoise"
-			   	$colour = :turquoise
-			   when "tyrian"
-			   	$colour = :tyrian
-			   when "brown"
-			   	$colour = :brown
-			   end
-			end
-
-			#if id == Gosu::KbUp then $size += 1 end
-			#if id == Gosu::KbDown then $size -= 1 end
-
-			if id == Gosu::KbRight then
-				print "Brush Size: "
-		        $size = gets.chomp.to_f
-		    end
+		        if id == Gosu::KbUp then $size += 1 end
+		        if id == Gosu::KbDown then $size -= 1 end
+		        	
 		    if id == Gosu::KbF then
 		    puts "Colours: green, red, blue, black, orange, cyan, purple, yellow, turquoise, tyrian, brown, yellow"
 			   print "What colour: "
@@ -146,16 +85,20 @@ class Window < Gosu::Window
 			   when "brown"
 			   	$colour1 = :brown
 			   end
-			   @background.paint {
-			      circle 20, 0, 5000, :color => $colour1, :fill => true
-		       }
 		    end
 		end
+		
+                @background.paint {
+		      circle 20, 0, 5000, :color => $colour1, :fill => true
+		 }
+		 
 		if button_down? Gosu::MsLeft then
 		@paint = true
 		@deleting = false
 		@background.paint {
 			circle $x, $y, $size, :color => $colour, :fill => true
+			
+			#Enable this to enable Vertical Line drawing
 			#line $x, $y, $x, $size, :color => $colour, :fill => true
  		}
 	    end
